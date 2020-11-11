924ebabf3ecaf6228b2ee3b12eea71a876173e6c
Tony Lampel <tla@xima.de>
2020-11-11 16:21:35 +0100

    [Task] Added ext_emconf.

---
+++ ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+
+/** @var string $_EXTKEY */
+$EM_CONF[$_EXTKEY] = [
+    'title'            => 'xm-viewhelper',
+    'description'      => 'Collection of custom viewhelper.',
+    'category'         => 'plugin',
+    'author'           => 'Tony Lampel',
+    'author_email'     => 'tony.lampel@xima.de',
+    'state'            => 'stable',
+    'internal'         => '',
+    'uploadfolder'     => '0',
+    'createDirs'       => '',
+    'clearCacheOnLoad' => 0,
+    'version'          => '0.1.0',
+    'constraints'      => [
+        'depends'   => [
+            'typo3'               => '8.7.0-8.7.99',
+        ],
+        'conflicts' => [],
+        'suggests'  => [],
+    ],
+];
