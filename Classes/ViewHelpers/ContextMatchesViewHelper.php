e8049a7c9cee1b1f7637267d19d50957def32384
Tony Lampel <tla@xima.de>
2020-12-16 08:41:43 +0100

    [Task] Added ContextMatchesViewHelper.

---
+++ Classes/ViewHelpers/ContextMatchesViewHelper.php
@@ -0,0 +1,57 @@
+<?php
+namespace Xima\XmViewhelper\ViewHelpers;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Class ContextMatchesViewHelper
+ *
+ * Returns true if the given context matches the current Application Context
+ *
+ * @see https://github.com/xima-media/xm-viewhelper/wiki/ContextMatchesViewHelper
+ *
+ * @example {xmvh:contextMatches(contexts: {1: 'Production/Standby'})}
+ *
+ * @package Xima\XmViewhelper\ViewHelpers
+ * @author Tony Lampel <tony.lampel@xima.de>
+ */
+class ContextMatchesViewHelper extends AbstractViewHelper
+{
+    /**
+     * Arguments Initialization
+     */
+    public function initializeArguments()
+    {
+        $this->registerArgument('contexts', 'array',
+            'Array of contexts like Production/Standby', false);
+    }
+
+    /**
+     * Context matches
+     *
+     * @return string
+     * @throws \Exception
+     */
+    public function render()
+    {
+        $contexts = (array) $this->arguments['contexts'];
+
+        /** @var ConfigurationManager configurationManager */
+        $configurationManager = GeneralUtility::makeInstance(ConfigurationManager::class);
+        $extbaseFrameworkConfiguration = $configurationManager
+            ->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
+
+        $settings = $extbaseFrameworkConfiguration['plugin.']['tx_xmviewhelper.']['settings.']['contextMatches.'];
+
+        if (empty($contexts)) {
+            $contexts = $settings['contexts'];
+        }
+
+        $context = GeneralUtility::getApplicationContext()->__toString();
+
+        return in_array($context, $contexts);
+    }
+}
