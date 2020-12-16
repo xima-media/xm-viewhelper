96185eb9cc2d13adf6329d6d8e854aba9eed2c74
Tony Lampel <tla@xima.de>
2020-12-16 08:39:18 +0100

    [Task] Removed ContentByContextViewHelper.

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -1,7 +1,6 @@
 plugin.tx_xmviewhelper {
     settings {
-        contentByContext {
-            partial2 = EXT:xm_viewhelper/Resources/Private/Partials/ContentByContext/Partial2.html
+        contextMatches {
             contexts {
             }
         }
