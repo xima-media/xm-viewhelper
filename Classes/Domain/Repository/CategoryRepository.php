6dfe5b459357cc61660395f6160babd810f1fcc7
Tony Lampel <tla@xima.de>
2021-02-04 16:44:38 +0100

    [Task] Migrated TYPO3_DB to doctrine dbal.

--- Classes/Domain/Repository/CategoryRepository.php
+++ Classes/Domain/Repository/CategoryRepository.php
@@ -1,6 +1,9 @@
 <?php
 namespace Xima\XmViewhelper\Domain\Repository;
 
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 /**
  * Class CategoryRepository
  *
@@ -8,38 +11,31 @@ namespace Xima\XmViewhelper\Domain\Repository;
  */
 class CategoryRepository extends \TYPO3\CMS\Extbase\Domain\Repository\CategoryRepository
 {
+    const SYS_CATEGORY_TABLE_NAME = 'sys_category';
+    const SYS_CATEGORY_MM_TABLE_NAME = 'sys_category_record_mm';
+
     /**
      * Get categories of a certain record
      *
      * @param string $table Table of the record to which categories are attached
-     * @param string $fieldname Filedname where the categories are stored
+     * @param string $fieldname Field name where the categories are stored
      * @param int $uid Uid of the record
      * @return array
      */
     public function findByTableAndFieldname(string $table, string $fieldname, int $uid)
     {
-        $where = 'AND sys_category_record_mm.fieldname = '
-            . $GLOBALS['TYPO3_DB']->fullQuoteStr($fieldname, 'sys_category_record_mm')
-            . ' AND sys_category_record_mm.uid_foreign = ' . (int)$uid;
+        /** @var \TYPO3\CMS\Core\Database\Query\QueryBuilder $queryBuilder */
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable(self::SYS_CATEGORY_TABLE_NAME);
+        $statement = $queryBuilder
+            ->select('local.*')
+            ->from(self::SYS_CATEGORY_TABLE_NAME, 'local')
+            ->join('local', self::SYS_CATEGORY_MM_TABLE_NAME, 'mm', 'mm.uid_local = local.uid')
+            ->join('mm', $table, 'foreign', 'foreign.uid = mm.uid_foreign')
+            ->where(
+                $queryBuilder->expr()->eq('mm.fieldname', $queryBuilder->createNamedParameter($fieldname))
+            )
+            ->execute();
 
-        $where .= ' AND (sys_category.sys_language_uid = ' . (int)$GLOBALS['TSFE']->sys_language_uid
-            . ' OR sys_category.sys_language_uid = -1)';
-
-        $res = $GLOBALS['TYPO3_DB']->exec_SELECT_mm_query(
-            'sys_category.*',
-            'sys_category',
-            'sys_category_record_mm',
-            $table,
-            $where,
-            '',
-            'sys_category_record_mm.sorting_foreign'
-        );
-
-        $rows = [];
-        while ($row = $res->fetch_assoc()) {
-            $rows[] = $row;
-        }
-
-        return $rows;
+        return $statement->fetchAll();
     }
 }
